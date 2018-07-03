25ef922a5ea6c5bb7179d8dc533cdc3012b3ba97
Matthew Hillman <mhillman@bomgar.com>
2018-07-02 22:02:58 -0500

    Better compare

--- commands/tw/compare.js
+++ commands/tw/compare.js
@@ -14,21 +14,40 @@ const queryGuilds = async (client, guild1, guild2) => {
   };
 
 exports.run = async (client, message, args) => {
-    console.warn(args);
-    const response = await queryGuilds(client, args[0], args[1]);
+
+    const waitingMessage = await message.channel.send("Querying");
+
+    await waitingMessage.react('⏳');
+
+    const response = await queryGuilds(client, args.first(), args.last());
+
+    await waitingMessage.react('🎉');
+    await waitingMessage.delete();
+    console.warn(response);
     if (response.error) {
         message.reply(response.error);
         return;
     }
     let msg = `= ${Object.keys(response).join(' vs ')} =\n`;
 
+    const g1Key = Object.keys(response).first();
+    const g2Key = Object.keys(response).last();
+    const winner = {};
+    ['zetas', 'gear_12', 'gear_11'].forEach((key) => {
+        winner[key] = +response[g1Key][key] > +response[g2Key][key] ? g1Key : g2Key;
+    });
+    winner['gear_11_12'] = (+response[g1Key]['gear_12'] + +response[g1Key]['gear_11']) > (+response[g2Key]['gear_12'] + +response[g2Key]['gear_11']) ? g1Key : g2Key;
+
     Object.keys(response).forEach((key) => {
         msg += `\n== ${key} ==\n`;
-        msg += `Zetas   :: ${response[key].zetas}\n`;
-        msg += `Gear 12 :: ${response[key].gear_12}\n`;
-        msg += `Gear 11 :: ${response[key].gear_11}\n`;
-        msg += `G 11+12 :: ${response[key].gear_12 + response[key].gear_11}\n`;
-        msg += `\n`;
+        let decorator = winner.zetas == key ? '**' : '';
+        msg += `Zetas   :: ${decorator}${response[key].zetas}${decorator}\n`;
+        decorator = winner.gear_12 == key ? '**' : '';
+        msg += `Gear 12 :: ${decorator}${response[key].gear_12}${decorator}\n`;
+        decorator = winner.gear_11 == key ? '**' : '';
+        msg += `Gear 11 :: ${decorator}${response[key].gear_11}${decorator}\n`;
+        decorator = winner.gear_11_12 == key ? '**' : '';
+        msg += `G 11+12 :: ${decorator}${response[key].gear_12 + response[key].gear_11}${decorator}\n`;
     });
 
     message.channel.send(msg, {code: 'asciidoc'});
