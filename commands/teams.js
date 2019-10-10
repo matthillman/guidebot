5e2bbf1a25655bc26845d66c5f6252eee025dc84
Matthew Hillman <mhillman@bomgar.com>
2019-10-09 23:33:42 -0500

    Add ability to parse people by discord id

--- commands/teams.js
+++ commands/teams.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const { snapshot } = require('../util/snapshot');
 const { Attachment } = require('discord.js');
 
@@ -10,6 +11,20 @@ const teamList = [
     {label: 'Hoth TB', value: 'tb'},
     {label: 'STR', value: 'str'},
 ];
+
+const getUserFromMention = async (client, mention) => {
+	if (!mention) return;
+
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+
+		return await client.fetchUser(mention);
+	}
+};
 
 exports.run = async (client, message, [team, allyCode]) => {
     if (!team) {
@@ -25,17 +40,38 @@ ${teamList.reduce((prev, team) => `${prev}${team.value}${' '.repeat(10 - team.va
 
     allyCode = allyCode.replace(/\-/g, '');
 
-    if (!/^[0-9]{9}$/.test(allyCode)) {
+    let realAllyCode = null;
+    if (/^[0-9]{9}$/.test(allyCode)) {
+        realAllyCode = allyCode;
+    } else if (/^<@\d+>$/.test(allyCode) || allyCode === 'me') {
+        const user = allyCode === 'me' ? message.author : await getUserFromMention(client, allyCode);
+        if (user) {
+            const response = await client.axios.get(`/api/registration/${user.id}`, {
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                })
+            });
+            realAllyCode = response.data.get.filter(obj => obj.discordId === user.id).map(obj => obj.allyCode);
+        }
+    }
+
+    if (realAllyCode === null) {
         return message.reply(`${allyCode} does not appear to be a valid ally code`);
     }
     await message.react('⏳');
 
-    const URL = `${client.config.client.base_url}/member/${allyCode}/${team}`;
+    if (!Array.isArray(realAllyCode)) {
+        realAllyCode = [realAllyCode];
+    }
 
-    const buffer = await snapshot(URL);
+    for (const code of realAllyCode) {
+        const URL = `${client.config.client.base_url}/member/${code}/${team}`;
+        const buffer = await snapshot(URL);
+        await message.channel.send(new Attachment(buffer, `${code}.png`));
+    }
 
     await message.react('🎉');
-    return message.channel.send(new Attachment(buffer, `${allyCode}.png`));
+    return;
 };
 
 exports.conf = {
