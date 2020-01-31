a623594fbf08abde44006d301e6ea0c793ba4b64
Matthew Hillman <mhillman@bomgar.com>
2020-01-31 16:05:37 -0600

    New compare command

--- commands/tw/compare.js
+++ commands/tw/compare.js
@@ -1,222 +1,57 @@
-const https = require('https');
-const { initImageFunctions, generateImage } = require('../../util/image-functions');
-const { Attachment } = require('discord.js');
+const { snapReplyForGuilds, scrapeGuild } = require('../../util/snapshot');
 
-const OPEN_EMPHASIS = '*';
-const CLOSE_EMPHASIS = '*';
+exports.run = async (client, message, [scrape, guild1, guild2]) => {
+    const doScrape = scrape == "--scrape" || scrape == "-s" || scrape == "—scrape";
 
-const formatGP = (gp) => {
-    return `${(gp / 1000000).toFixed(1)}M`;
-};
-
-const docBlock = (lines, lang) => {
-    let value = `\`\`\`${lang || 'asciidoc'}\n`;
-    value += Array.isArray(lines) ? [].concat(...lines).join('\n') : lines;
-    value +=`\`\`\``;
-
-    return value;
-};
-
-const decorate = (key, value, padding, isWinner) => {
-    return `${key}${' '.repeat(padding - key.length)} :: ${isWinner ? OPEN_EMPHASIS : '' }${value}${isWinner ? CLOSE_EMPHASIS : '' }`;
-};
-
-const queryGuilds = async (client, guild1, guild2) => {
-    try {
-        const response = await client.axios.get(`/api/tw/compare/${guild1}/${guild2}`, {
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false
-            })
-        });
-        return response.data;
-    } catch (error) {
-        return {
-            error: error
-        };
-    }
-};
-
-const scrapeGuild = async (client, id, callback) => {
-    try {
-        client.guildQueue.push({
-            id,
-            callback,
-        });
-        const response = await client.axios.get(`/api/guild/scrape/${id}`, {
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false,
-            })
-        });
-
-        return response;
-    } catch (error) {
-        return { error };
-    }
-};
-
-const failed = [];
-
-const doQuery = async (client, message, args) => {
-    const waitingMessage = await message.channel.send("Querying " + args.join(' vs '));
-    await waitingMessage.react('⏳');
-
-    let response;
-    let parsedArgs;
-
-    if (args.first() == "--scrape") {
-        parsedArgs = [args[1], args[2]];
-        response = {
-            error: true,
-            both: true,
-        };
-    } else {
-        response = await queryGuilds(client, args.first(), args.last());
-        parsedArgs = args;
-
-        await waitingMessage.react('🎉');
+    if (!doScrape) {
+        guild2 = guild1;
+        guild1 = scrape;
     }
 
-    await waitingMessage.delete();
+    if (guild1) {
+        guild1 = guild1.replace(/\-/g, '');
+    }
 
-    if (response.error) {
-        if (response[parsedArgs.first()] || response[parsedArgs.last()] || response.both) {
-            const needsScrape = response.both ? parsedArgs : response[parsedArgs.first()] ? [parsedArgs.first()] : [parsedArgs.last()];
+    if (guild2) {
+        guild2 = guild2.replace(/\-/g, '');
+    }
 
-            await needsScrape.forEach(async (scrape, index) => {
+    if (!(/^[0-9]{1,9}$/.test(guild1))) {
+        await message.react('🤔');
+        return message.reply(`${guild1} does not appear to be a valid guild id or ally code`);
+    }
+    if (!(/^[0-9]{1,9}$/.test(guild2))) {
+        await message.react('🤔');
+        return message.reply(`${guild2} does not appear to be a valid guild id or ally code`);
+    }
 
-                if (failed.indexOf(scrape) != -1) {
-                    return await message.reply(`Querying guild ${scrape} has failed too many times. Please manually scrape this guild and try again`);
-                }
+    await message.react('⏳');
 
-                failed.push(scrape);
-                const scrapeMessage = await message.channel.send(`Guild ${scrape} needs to be scraped first…`);
-                await scrapeMessage.react('⏳');
-                await scrapeGuild(client, scrape, async () => {
+    if (doScrape) {
+        const scrapeMessage = await message.channel.send(`Trying to scrape guilds ${guild1} and ${guild2}…`);
+        await scrapeMessage.react('⏳');
+        let completeCount = 0;
+
+        [guild1, guild2].forEach(async code => {
+            await scrapeGuild(client, code, async () => {
+                completeCount += 1;
+
+                if (completeCount == 2) {
                     await scrapeMessage.react('🎉');
                     await scrapeMessage.delete();
 
-                    if (index == needsScrape.length - 1) {
-                        return doQuery(client, message, parsedArgs);
-                    }
-                });
-            });
-
-            return;
-        } else {
-            client.logger.log(Object.values(response));
-            message.reply(response.error);
-            return;
-        }
+                    await snapReplyForGuilds(guild1, guild2, `compare`, message, client);
+                } else {
+                    await scrapeMessage.react('⌛️');
+                }
+            })
+        });
+    } else {
+        await snapReplyForGuilds(guild1, guild2, `compare`, message, client);
     }
 
-    failed.remove(args.first());
-    failed.remove(args.last());
-
-    const g1Key = Object.keys(response).first();
-    const g2Key = Object.keys(response)[1];
-    const msg = `= ${[g1Key, g2Key].join(' vs ')} =`;
-
-    const charKeys = response.char_keys;
-    // const charNames = response.char_names;
-    const modKeys = response.mod_keys;
-
-    const winner = {};
-
-    ['gp', 'zetas', 'gear_13', 'gear_12', 'gear_11', 'relic_7', 'relic_6', 'relic_5'].forEach(key => {
-        winner[key] = +response[g1Key][key] > +response[g2Key][key] ? g1Key : g2Key;
-    });
-    Object.keys(modKeys).forEach(key => {
-        winner[key] = +response[g1Key].mods[key] > +response[g2Key].mods[key] ? g1Key : g2Key;
-    });
-    winner['gear_11_12'] = (+response[g1Key]['gear_13'] + +response[g1Key]['gear_12'] + +response[g1Key]['gear_11']) > (+response[g2Key]['gear_13'] + +response[g2Key]['gear_12'] + +response[g2Key]['gear_11']) ? g1Key : g2Key;
-
-    const longest = ['Members', 'GP', 'Zetas'].reduce((long, str) => Math.max(long, str.length), 0);
-    const gpFields = [g1Key, g2Key].map((key) => {
-        const name = `${key}`;
-        const value = docBlock([
-            decorate('Members', response[key].member_count, longest),
-            decorate('GP', formatGP(response[key].gp), longest, winner.gp == key),
-            decorate('Zetas', response[key].zetas, longest, winner.zetas == key),
-        ]);
-
-        return { name, value, inline: true };
-    });
-
-    const longestGear = ['Gear 13', 'Gear 12', 'Gear 11', 'G 11+', 'Relic 7', 'Relic 6', 'Relic 5'].reduce((long, str) => Math.max(long, str.length), 0);
-    const gearFields = [g1Key, g2Key].map(key => {
-        const name =`${key}`;
-        const value = docBlock([
-            decorate('Gear 13', response[key].gear_13, longestGear, winner.gear_13 == key),
-            decorate('Gear 12', response[key].gear_12, longestGear, winner.gear_12 == key),
-            decorate('Gear 11', response[key].gear_11, longestGear, winner.gear_11 == key),
-            decorate('G 11+', response[key].gear_13 + response[key].gear_12 + response[key].gear_11, longestGear, winner.gear_11_12 == key),
-            decorate('Relic 7', response[key].relic_7, longestGear, winner.relic_7 == key),
-            decorate('Relic 6', response[key].relic_6, longestGear, winner.relic_6 == key),
-            decorate('Relic 5', response[key].relic_5, longestGear, winner.relic_5 == key),
-        ]);
-
-        return { name, value, inline: true };
-    });
-
-	const longestMod = Object.values(modKeys).reduce((long, str) => Math.max(long, str.length), 0);
-    const modFields = [g1Key, g2Key].map((key) => {
-        const name = `${key}`;
-        const guildMods = response[key].mods;
-        const value = docBlock(
-            Object.keys(modKeys).map(mod => decorate(modKeys[mod], guildMods[mod], longestMod, winner[mod] == key))
-        );
-
-        return { name, value, inline: true };
-    });
-
-    const guilds = [g1Key, g2Key].map(key => {
-        const r = {};
-        charKeys.forEach(char => {
-            r[char] = {
-                base_id: `${char}`,
-                gear_11: response[key][`${char}_11`],
-                gear_12: response[key][`${char}_12`],
-                gear_13: response[key][`${char}_13`],
-                relic_total: response[key][`${char}_r_total`],
-                relic_5: response[key][`${char}_r5`],
-                relic_6: response[key][`${char}_r6`],
-                relic_7: response[key][`${char}_r7`],
-            };
-        });
-        return r;
-    });
-
-    const charImageCanvas = await generateImage(guilds, g1Key, g2Key);
-    const charImage = new Attachment(charImageCanvas.toBuffer(), `compare.png`);
-
-    await message.channel.send({embed: {
-        author: { name: msg },
-        color: 0xfce34d,
-        url: 'https://schwartz.hillman.me/home',
-        thumbnail: {
-            url: 'https://schwartz.hillman.me/images/Logo@2x.png',
-        },
-        description: `Fuckin' kill 'em`,
-        fields: [].concat.apply([], [
-            gpFields,
-            { value: docBlock('= Gear ='), name: '\u200b' },
-            gearFields,
-            { value: docBlock('= Mods ='), name: '\u200b' },
-            modFields,
-        ]),
-        timestamp: new Date(),
-        footer: {
-            text: 'May the Schwartz be with you',
-            icon_url: 'https://schwartz.hillman.me/images/schwartz.jpg',
-        },
-    }});
-    await message.channel.send(charImage);
-};
-
-exports.run = doQuery;
-
-exports.init = async (client) => {
-    await initImageFunctions(client);
+    await message.react('🎉');
+    return;
 };
 
 exports.conf = {
@@ -229,6 +64,6 @@ exports.conf = {
 exports.help = {
     name: "compare",
     category: "SWGOH",
-    description: "Compares the high level stats of two guilds. Note guild\_id is the id from the URL in their swogh profile.",
-    usage: "compare [guild_id 1] [guild_id 2]"
+    description: "Compares two guilds",
+    usage: "compare [guild id or ally code] [guild id or ally code]"
 };
--- util/snapshot.js
+++ util/snapshot.js
@@ -44,6 +44,24 @@ const scrapeUser = async (client, code, callback) => {
     }
 };
 
+const scrapeGuild = async (client, code, callback) => {
+    try {
+        client.userQueue.push({
+            code,
+            callback,
+        });
+        const response = await client.axios.get(`/api/guild/scrape/${code}`, {
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false,
+            })
+        });
+
+        return response;
+    } catch (error) {
+        return { error };
+    }
+};
+
 const reallyDoSnap = async (URL, message, name) => {
     const buffer = await snapshot(URL);
     await message.channel.send(new Attachment(buffer, `${name}.png`));
@@ -79,11 +97,55 @@ const snapReplyForAllyCodes = async (codes, urlSlug, message, client, urlSuffix)
                 await scrapeMessage.react('🎉');
                 await scrapeMessage.delete();
 
-                snapReplyForAllyCodes(codes, urlSlug, message, client);
+                await snapReplyForAllyCodes(codes, urlSlug, message, client);
             });
         }
     }
 };
 
+const snapReplyForGuilds = async (guild1, guild2, urlSlug, message, client, urlSuffix) => {
+    const originalArgs = [].slice.call(arguments);
+    const combinedID = `${guild1}vs${guild2}`;
+    const failIndex = failed.indexOf(combinedID);
+    const URL = `${client.config.client.base_url}/${urlSlug}/${guild1}/${guild2}${urlSuffix || ''}`;
+    try {
+        await reallyDoSnap(URL, message, combinedID);
+
+        if (failIndex > -1) {
+            failed.slice(failIndex, 1);
+        }
+    } catch (e) {
+        client.logger.error(`Fetching page to snapshot failed with status ${e.message} (${URL})`);
+        if (failIndex > -1) {
+            failed.slice(failIndex, 1);
+            await message.reply(`Querying guilds ${guild1} + ${guild2} has failed too many times. Please manually scrape this user and try again`);
+        }
+        failed.push(combinedID);
+        client.logger.error(`Error getting snapshot for ${urlSlug} -> ${combinedID}, trying to query`);
+        const scrapeMessage = await message.channel.send(`Trying to scrape guilds ${guild1} and ${guild2}…`);
+        await scrapeMessage.react('⏳');
+        let completeCount = 0;
+
+        [guild1, guild2].forEach(async code => {
+            await scrapeGuild(client, code, async () => {
+                completeCount += 1;
+
+                if (completeCount == 2) {
+                    await scrapeMessage.react('🎉');
+                    await scrapeMessage.delete();
+
+                    await snapReplyForGuilds(guild1, guild2, `compare`, message, client);
+                } else {
+                    await scrapeMessage.react('⌛️');
+                }
+            })
+        });
+    }
+
+};
+
 exports.snapshot = snapshot;
 exports.snapReplyForAllyCodes = snapReplyForAllyCodes;
+exports.snapReplyForGuilds = snapReplyForGuilds;
+exports.scrapeGuild = scrapeGuild;
+exports.scrapeUser = scrapeUser;
