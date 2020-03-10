c30fcb85dc0533de3138f94e6d725afd4422fd8e
Matthew Hillman <mhillman@bomgar.com>
2020-03-10 09:26:16 -0500

    Don't set a status if it's the debug bot

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,8 @@ module.exports = async client => {
   // Log that the bot is online.
   client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
-  // Make the bot "play the game" which is the help command with default prefix.
-  client.user.setActivity(`${client.config.defaultSettings.prefix}help (${client.guilds.size} servers)`, {type: "PLAYING"});
+  if (!client.config.debug) {
+    // Make the bot "play the game" which is the help command with default prefix.
+    client.user.setActivity(`${client.config.defaultSettings.prefix}help (${client.guilds.size} servers)`, {type: "PLAYING"});
+  }
 };
