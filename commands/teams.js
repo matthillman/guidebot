35b4866fb940e4e18344f556e12c021eb860bbc5
Matthew Hillman <mhillman@bomgar.com>
2020-12-12 09:09:45 -0600

    Fix for missing ally code parsing

--- commands/teams.js
+++ commands/teams.js
@@ -17,7 +17,7 @@ const teamList = [
 exports.run = async (client, message, [allyCode, team]) => {
     const firstArgAppearsToBeAllyCode = /^[0-9]{9}$/.test(allyCode) || /^<@.+>$/.test(allyCode) || allyCode === 'me';
 
-    if (team.length === 0 || !firstArgAppearsToBeAllyCode) {
+    if (!team || team.length === 0 || !firstArgAppearsToBeAllyCode) {
         // assuming that if we only get 1 argument it's a character name
         team = allyCode;
         allyCode = null;
